04ce1dbc192fc0b07a942096cb213d85a367e020
sandshoes <healy.tomas@gmail.com>
2023-05-18 14:59:39 +0200

    minor changes

--- backend/src/services/event.service.ts
+++ backend/src/services/event.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma, PrismaClient, events } from '@prisma/client';
+import { PrismaClient, events } from '@prisma/client';
 import { aggregatorMap, categoricalMood } from 'src/utils/helper';
 
 @Injectable()
